822532da22b534623e73257b5a1df2b140710ec1
srboc <sr.boc.naturalstyle@icloud.com>
2024-04-18 00:29:06 +0900

    modify:index.html

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.template import loader
 from .models import Question
 
 
@@ -20,5 +21,8 @@ def vote(request, question_id):
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    output = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    template = loader.get_template("polls/index.html")
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
