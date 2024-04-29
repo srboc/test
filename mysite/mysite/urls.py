a6fc9c4e4e5cb955cdceb34342751d72fe1371fe
srboc <sr.boc.naturalstyle@icloud.com>
2024-04-29 22:42:52 +0900

    modify :url.py

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,10 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-app_name = "polls"
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("<int:question_id>/", views.detail, name="detail"),
-    path("<int:question_id>/results/", views.results, name="results"),
-    path("<int:question_id>/vote/", views.vote, name="vote"),
+    path("polls/", include("polls.urls")),
+    path("admin/", admin.site.urls),
 ]
